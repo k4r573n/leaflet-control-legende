c04e6e1b1dde5e7a2a0f40c827f6c0541d321d7e
Karsten Hinz <k.hinz@tu-bs.de>
2013-03-17 16:35:43 +0100

    running version

--- Control.OSMGeocoder.js
+++ Control.OSMGeocoder.js
@@ -1,15 +1,15 @@
 L.Control.OSMGeocoder = L.Control.extend({
 	options: {
 		collapsed: true,
-		position: 'topright',
-		text: 'Locate',
+		position: 'bottomleft',
+		tooltip: 'Show info',
 		callback: function (results) {
-      console.log(results);
-			var bbox = results[0].boundingbox,
-				first = new L.LatLng(bbox[0], bbox[2]),
-				second = new L.LatLng(bbox[1], bbox[3]),
-				bounds = new L.LatLngBounds([first, second]);
-			this._map.fitBounds(bounds);
+      text = "";
+      for (i=0; i<results.length;i++) {
+        text += '<div><img src="images/conTag_'+results[i].id+'.png"/>'+
+                '<b>'+results[i].name+'</b><br>'+results[i].desc+'</div>';
+      }
+      this._element.innerHTML = text;
 		}
 	},
 
@@ -21,59 +21,45 @@ L.Control.OSMGeocoder = L.Control.extend({
 
 	onAdd: function (map) {
 		this._map = map;
-		var className = 'leaflet-control-geocoder',
+		var className = 'leaflet-control-legende',
 			container = this._container = L.DomUtil.create('div', className);
 
 		L.DomEvent.disableClickPropagation(container);
 
-		var form = this._form = L.DomUtil.create('form', className + '-form');
+		var display = this._element = L.DomUtil.create('div', className + '-div');
+    display.innerHTML="Content is loading...";
+    L.DomEvent.addListener(display, 'click', this._toggle, this);
 
-		var input = this._input = document.createElement('input');
-		input.type = "text";
+    var link = this._link = document.createElement('a');
+    link.href = '#';
+    link.title = this.options.tooltip;
 
-		var submit = document.createElement('button');
-		submit.type = "submit";
-		submit.innerHTML = this.options.text;
-
-		form.appendChild(input);
-		form.appendChild(submit);
-
-		L.DomEvent.addListener(form, 'submit', this._geocode, this);
+    L.DomEvent.addListener(link, 'click', this._toggle, this);
 
 		if (this.options.collapsed) {
-			L.DomEvent.addListener(container, 'mouseover', this._expand, this);
-			L.DomEvent.addListener(container, 'mouseout', this._collapse, this);
+      display.style.display = 'none';
+		}else {
+      link.style.display = 'none';
+    }
 
-			var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
-			link.href = '#';
-			link.title = 'Nominatim Geocoder';
+		container.appendChild(display);
+		container.appendChild(link);
 
-			L.DomEvent.addListener(link, L.Browser.touch ? 'click' : 'focus', this._expand, this);
-
-			this._map.on('movestart', this._collapse, this);
-		} else {
-			this._expand();
-		}
-
-		container.appendChild(form);
+    this._getTags();
 
 		return container;
 	},
 
-	_geocode : function (event) {
-		L.DomEvent.preventDefault(event);
-    //http://wiki.openstreetmap.org/wiki/Nominatim
-		this._callbackId = "_l_osmgeocoder_" + (this._callbackId++);
+	_getTags : function () {
+		this._callbackId = "_l_inbazlegende_" + (this._callbackId++);
 		window[this._callbackId] = L.Util.bind(this.options.callback, this);
 
 		var params = {
-			q: this._input.value,
-			json_callback : this._callbackId,
-      format: 'json'
-		},
-		url = " http://nominatim.openstreetmap.org/search" + L.Util.getParamString(params),
+			callback : this._callbackId,
+      getTags: 'yes'
+		};
+		url = "http://bastler.bplaced.net/api/inbaz_api.php" + L.Util.getParamString(params);
 		script = document.createElement("script");
-
 
 		script.type = "text/javascript";
 		script.src = url;
@@ -81,11 +67,19 @@ L.Control.OSMGeocoder = L.Control.extend({
 		document.getElementsByTagName("head")[0].appendChild(script);
 	},
 
-	_expand: function () {
-		L.DomUtil.addClass(this._container, 'leaflet-control-geocoder-expanded');
-	},
+  _toggle: function (event) {
+		L.DomEvent.preventDefault(event);
+    //toggle info display
+		if (this.options.collapsed) {
+      this._element.style.display = 'block';
+      this._link.style.display = 'none';
+      this.options.collapsed = false;
+    }else {
+      this._element.style.display = 'none';
+      this._link.style.display = 'block';
+      this.options.collapsed = true;
+    }
 
-	_collapse: function () {
-		this._container.className = this._container.className.replace(' leaflet-control-geocoder-expanded', '');
-	}
+  }
+
 });
